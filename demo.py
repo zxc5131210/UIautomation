bc03e271b40f88410382b0b9de09fe69bb15313d
ian.wu <ian.viewsonic@gmail.com>
2023-08-22 11:35:57 +0800

    0822

--- demo.py
+++ demo.py
@@ -22,17 +22,16 @@ driver = webdriver.Remote(f'http://{config.host}:{config.port}/wd/hub', desired_
 
 #Step 3 : Create "gesture automation flow"
 gesture = Gesture(driver=driver)
-event_gen = EventGen()
-event_gen.generate_event(json_path='./motion_flow.json', driver=driver)
+# event_gen = EventGen()
+# event_gen.generate_event(json_path='./motion_flow.json', driver=driver)
 
+#step1 : drag and drop
 """swipe to open the all apps view"""
 gesture.swipe_up()
 
-# drag and drop demo
 try:
-    drag_ele = driver.find_element(AppiumBy.XPATH,'	//android.widget.TextView[@content-desc="Phone"]')
-    drop_ele = driver.find_element(AppiumBy.XPATH , '//android.widget.TextView[@content-desc="Calendar"]')
-    gesture.drag_drop_byelement(drag_ele, drop_ele)
+    drag_ele = driver.find_element(AppiumBy.ACCESSIBILITY_ID,'Phone')
+    gesture.drag_drop_bylocate(drag_ele,534,721)
     time.sleep(3)
     logger.debug(f'drag {drag_ele} on desktop success')
 
@@ -41,13 +40,14 @@ except :
 
 '''verify App is on the desktop'''
 try:
-    driver.find_element(AppiumBy.XPATH,'//android.widget.TextView[@content-desc="Phone"]')
+    driver.find_element(AppiumBy.ACCESSIBILITY_ID,'Phone')
     logger.debug(f'{drag_ele} is on desktop')
 except:
     logger.debug(f'{drag_ele} is not on desktop')
 
+#step2 : move to second view
 '''move the app form the first view to the second view'''
-drag_ele = driver.find_element(AppiumBy.XPATH,'	//android.widget.TextView[@content-desc="Phone"]')
+drag_ele = driver.find_element(AppiumBy.ACCESSIBILITY_ID,'Phone')
 gesture.drag_drop_bylocate(drag_ele,1068,832)
 
 '''verify the app is on second view or not'''
@@ -60,3 +60,34 @@ try:
 
 except:
     logger.debug('This is the second view')
+
+#step3 :Tap the app on hot seat ,and check if app started
+'''verify the app is on the hot seat'''
+message_app = driver.find_element(AppiumBy.ACCESSIBILITY_ID,'Messages')
+
+
+
+'''worst fuction to verify hot seat'''
+# # 獲取元素的位置
+# element_location = message_app.location
+# element_y = element_location['y']
+# print(element_y)
+
+# # 獲取屏幕高度
+# screen_size = driver.get_window_size()
+# screen_height = screen_size['height']
+
+
+# # 定義熱門應用欄的範圍（例如，底部10%的區域）
+# hotseat_range = 0.3 * screen_height
+# print(screen_height - hotseat_range)
+# # 判斷元素的 Y 坐標是否在熱門應用欄的範圍內
+# if element_y >= (screen_height - hotseat_range):
+#     print("The element is in the Hot Seat.")
+# else:
+#     print("The element is not in the Hot Seat.")
+
+
+
+
+driver.quit()
