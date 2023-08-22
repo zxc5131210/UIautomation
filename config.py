6cb848b127096eecb83faddeb7f957c5ad0365f8
ian.wu <ian.viewsonic@gmail.com>
2023-08-22 08:33:38 +0800

    0822

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 """Device setting"""
 
 platformName='Android'
-platformVersion='11'
-deviceName='VBS200'
+platformVersion='14'
+deviceName='emlator-5554'
 udid = 'emulator-5554'
 
 '''Appium Connect setting'''
